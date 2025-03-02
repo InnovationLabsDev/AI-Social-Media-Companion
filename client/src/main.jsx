5e97d186e5db0c8524cfc93f59af79ed95eb9979
alinandrei2004 <145998080+alinandrei2004@users.noreply.github.com>
2025-03-02 19:50:42 +0200

    [feat]: Add routes and frontpage
    Signed-off-by: alinandrei2004 <145998080+alinandrei2004@users.noreply.github.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import FrontPage from './pages-website/frontPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        {/* <Route path="/app" element={<App />} /> */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
